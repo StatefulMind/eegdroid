580c411d7f459fcf4f979fee26b830199d3055a4
Adrián Rojas Campos <adrocampos@Adrians-MacBook-Pro.local>
2018-11-19 10:59:03 +0100

    add BluetoothLEGatt

--- EEGDroid_new/app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Connect.java
+++ EEGDroid_new/app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/Connect.java
@@ -1,7 +1,13 @@
 package de.uni_osnabrueck.ikw.eegdroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class Connect extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class Connect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button scan_button = (Button)findViewById(R.id.scan_button);
+
+        scan_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Connect.this, DeviceScanActivity.class));
+            }
+        });
     }
+
 }
--- EEGDroid_new/app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/MainActivity.java
+++ EEGDroid_new/app/src/main/java/de/uni_osnabrueck/ikw/eegdroid/MainActivity.java
@@ -69,10 +69,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
+        if (id == R.id.Connect) {
+            Intent intent = new Intent(this, Connect.class);
             startActivity(intent);
-            return true;
         }
 
         return super.onOptionsItemSelected(item);
